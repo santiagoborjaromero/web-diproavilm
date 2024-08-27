a1dac618d606373811b3589c75602e035dcf757b
Santiago Borja Romero <santiago.borja@gmail.com>
2024-08-26 19:19:49 -0500

    Limpieza y creacion de htaccess para el server que es ignorado para no afectar otras configuraciones

--- src/Views/skeleton/skeleton.php
+++ src/Views/skeleton/skeleton.php
@@ -29,49 +29,5 @@
     </div>
 </div>
 
-<!-- <div id="DivForm" title="Diproavilm" class="hide">
-    <div class="modal-body">
-        <span id="MessageDivForm"></span>
-    </div>
-</div> -->
-
-
-<!-- <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h6>Diproavilm</h6>
-            </div>
-            <div class="modal-bodyasd" id="MessagemyModal">
-                <p>Some text in the modal.</p>
-            </div>
-            <div class="modal-footer" id="MessagemyFooter">
-                <button type="button" id="" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div> -->
-<!-- 
-<div class="modal fade" id="DivForm" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h6 class="modal-title" id="staticBackdropLabel">Diproavilm</h6>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form id="frm">
-                <div class="modal-body" id="MessageDivForm">
-                
-
-                </div>
-                <div class="modal-footer">
-                </div>
-            </form>
-        </div>
-    </div>
-</div> -->
-
-
-
 <script src="src/Views/skeleton/skeleton.js"></script>
 <link rel="stylesheet" href="src/Views/skeleton/skeleton.css">
