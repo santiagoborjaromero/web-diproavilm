e92fa1989ade084f1e294382043050bc58ffedbc
Santiago Borja Romero <santiago@callevo.com>
2024-09-21 19:48:42 -0500

    dash modificacion

--- src/Views/dashventas/dashventas.js
+++ src/Views/dashventas/dashventas.js
@@ -3,8 +3,7 @@ title = ruta.name;
 $("#Title").html(title);
 $("#TitleIcon").addClass(ruta.icon);
 
-console.log("aaaa")
-buscar();
+console.log("Cargando")
 goDash();
 
 function goDash() {
@@ -13,26 +12,3 @@ function goDash() {
     $("#grafana").attr("src", url_grafana);
 }
 
-
-function buscar(){
-    const ctx = document.getElementById('myChart');
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-        },
-        options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
-        }
-    });
-}
